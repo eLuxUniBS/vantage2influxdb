3a01996ef3c2b2ec368f381baa0e42bd3f29dddc
Paolo Bellagente <p.bellagente@unibs.it>
2017-03-21 15:31:20 +0100

    - bug fixing

--- vantageEntity.py
+++ vantageEntity.py
@@ -8,45 +8,45 @@ class VantageMeasure(object):
     def __init__(self, archiveRecord):
         # see the PyWeather davis_weatherLinkIP module for details about fields
         filters = {
-            'TempOut': self.set_FinC,
-            'TempOutHi': self.set_FinC,
-            'TempOutLow': self.set_FinC,
-            'RainRate': self.set_rain_in_mm,
-            'RainRateHi': self.set_rain_in_mm,
-            'Barometer': self.set_minHg_in_Pa,
-            'SolarRad': self.addParameter, # Average Solar Rad over the archive period. Units are (Watts / m 2 )
-            'WindSamps': self.skipValue,
-            'TempIn': self.set_FinC,
-            'HumIn': self.set_in_percent,
-            'HumOut': self.set_in_percent,
-            'WindAvg': self.set_mph_in_kph,
-            'WindHi': self.set_mph_in_kph,
-            'WindHiDir': self.winDir_in_Deg,
-            'WindAvgDir': self.winDir_in_Deg,
-            'UV': self.addParameter,
-            'ETHour': self.set_milliin_in_mm,
-            'SolarRadHi': self.addParameter,
-            'UVHi': self.addParameter,
-            'ForecastRuleNo': self.skipValue,
-            'LeafTemps': self.skipValue,
-            'LeafWetness': self.skipValue,
-            'SoilTemps': self.skipValue,
-            'RecType': self.skipValue,
-            'ExtraHum': self.skipValue,
-            'ExtraTemps': self.skipValue,
-            'SoilMoist': self.skipValue,
+            'TempOut': self._set_dF_in_C,
+            'TempOutHi': self._set_FinC,
+            'TempOutLow': self._set_FinC,
+            'RainRate': self._set_rain_in_mm,
+            'RainRateHi': self._set_rain_in_mm,
+            'Barometer': self._set_minHg_in_Pa,
+            'SolarRad': self._addParameter, # Average Solar Rad over the archive period. Units are (Watts / m 2 )
+            'WindSamps': self._skipValue,
+            'TempIn': self._set_FinC,
+            'HumIn': self._set_in_percent,
+            'HumOut': self._set_in_percent,
+            'WindAvg': self._set_mph_in_mps,
+            'WindHi': self._set_mph_in_mps,
+            'WindHiDir': self._winDir_in_Deg,
+            'WindAvgDir': self._winDir_in_Deg,
+            'UV': self._addParameter,
+            'ETHour': self._set_milliin_in_mm,
+            'SolarRadHi': self._addParameter,
+            'UVHi': self._addParameter,
+            'ForecastRuleNo': self._skipValue,
+            'LeafTemps': self._skipValue,
+            'LeafWetness': self._skipValue,
+            'SoilTemps': self._skipValue,
+            'RecType': self._skipValue,
+            'ExtraHum': self._skipValue,
+            'ExtraTemps': self._skipValue,
+            'SoilMoist': self._skipValue,
             # calculate values
-            'HeatIndex': self.set_FinC,
-            'Year': self.addParameter,
-            'Day': self.addParameter,
-            'Month': self.addParameter,
-            'Hour': self.addParameter,
-            'Min': self.addParameter,
-            'DateStamp': self.skipValue,
-            'DewPoint': self.set_FinC,
-            'WindChill': self.set_FinC,
-            'TimeStamp': self.skipValue,
-            'DateStampUtc': self.addParameter
+            'HeatIndex': self._set_FinC,
+            'Year': self._addParameter,
+            'Day': self._addParameter,
+            'Month': self._addParameter,
+            'Hour': self._addParameter,
+            'Min': self._addParameter,
+            'DateStamp': self._skipValue,
+            'DewPoint': self._set_FinC,
+            'WindChill': self._set_FinC,
+            'TimeStamp': self._skipValue,
+            'DateStampUtc': self._addParameter
         }
         # populate the object with all the parameters in archive record using the suitable filters
         for measure, value in archiveRecord.iteritems():
@@ -59,7 +59,7 @@ class VantageMeasure(object):
         self.__delattr__("Hour")
         self.__delattr__("Min")
 
-    def skipValue(self, measurement, value):
+    def _skipValue(self, measurement, value):
         '''
         This method implements a notch filter on the inserted parameters.
         :param measurement: the measurement name
@@ -67,7 +67,7 @@ class VantageMeasure(object):
         '''
         logging.info("the measurement " + measurement + " has been skipped")
 
-    def addParameter(self, measure, value):
+    def _addParameter(self, measure, value):
         self.__setattr__(measure, value)
 
     def jsonify(self, sourceTZ, measurement="Vantage"):
@@ -87,16 +87,25 @@ class VantageMeasure(object):
         return json_body
 
     ############################ FILTER IMPLEMENTATION ###############################
-    def set_FinC(self, measure, value):
+    def _set_FinC(self, measure, value):
         '''
         Add the measure in fahrenheit to the object as celsius value
         :param measure: name of the parameter to be added
         :param value: the temperature int fahrenheit
         :return: add parameter with name "measure" and value "value" in Celsius Degree to the object.
         '''
-        self.addParameter(measure, fahrenheit_to_celsius(value))
+        self._addParameter(measure, fahrenheit_to_celsius(value))
 
-    def set_minHg_in_Pa(self, measure, value):
+    def _set_dF_in_C(self, measure, value):
+        '''
+        Add the measure in fahrenheit/10 to the object as celsius value
+        :param measure: name of the parameter to be added
+        :param value: the temperature int fahrenheit
+        :return: add parameter with name "measure" and value "value" in Celsius Degree to the object.
+        '''
+        self._addParameter(measure, fahrenheit_to_celsius(value/10.0))
+
+    def _set_minHg_in_Pa(self, measure, value):
         '''
         Add the measure in milliinches of Hg to the object as Pascal value
         :param measure: name of the parameter to be added
@@ -104,9 +113,9 @@ class VantageMeasure(object):
         :return: add parameter with name "measure" and value "value"*1000 converted in Pascals using the NIST
         conventional conversion to the object.
         '''
-        self.addParameter(measure, incConv_to_Pa(value * 1000))
+        self._addParameter(measure, incConv_to_Pa(value * 1000))
 
-    def set_rain_in_mm(self, measure, value):
+    def _set_rain_in_mm(self, measure, value):
         '''
         Set the rainfall measure in mm
         :param measure: name of the parameter to be added
@@ -114,36 +123,41 @@ class VantageMeasure(object):
         :return: add parameter with name "measure" and value "value*0.2" to the object. 0.2 is the conversion rate of
         one click in mm for the vantagePro2 see Davis Spec sheet
         '''
-        self.addParameter(measure, value*0.2)
+        self._addParameter(measure, value * 0.2)
 
-    def set_in_percent(self, measure, value):
+    def _set_in_percent(self, measure, value):
         '''
         Add the paremtere "measure" with value 0-255 to percentage.
         :param measure: name of the parameter to be added
         :param value: value in a 0-255 integer interval
         :return: add a parameter with name measure and value "value" to the object
         '''
-        self.addParameter(measure, int(round(value*100.0/255)))
+        self._addParameter(measure, int(round(value * 100.0 / 255)))
 
-    def set_mph_in_kph(self, measure, value):
+    def _set_mph_in_mps(self, measure, value):
         '''
-        Add the paremtere "measure" with value in mph to kph.
+        Add the paremtere "measure" with value in mph to m/s.
         :param measure: name of the parameter to be added
         :param value: value in mph
-        :return: add a parameter with name measure and value "value" in kph to the object
+        :return: add a parameter with name measure and value "value" in m/s to the object
         '''
-        self.addParameter(measure, mph_to_km_hr(value))
+        self._addParameter(measure, mph_to_m_sec(value))
 
-    def winDir_in_Deg(self, measure, value):
+    def _winDir_in_Deg(self, measure, value):
         '''
         Add the wind direction constant converted into decimal degrees as parameter to the object
         :param measure: name of the parameter to be added
         :param value: value to be converted using the vantage wind direction protocol. See Davis serial protocol documentation.
         :return: add a parameter with name measure and value "value" in degrees to the object
         '''
-        self.addParameter(measure, value*22.5)
+        self._addParameter(measure, value * 22.5)
 
-    def set_milliin_in_mm(self, measure, value):
-        # todo: docs
+    def _set_milliin_in_mm(self, measure, value):
+        '''
+        Add amount of rain in mm as parameter to the object
+        :param measure: name of the parameter to be added
+        :param value: value in milli inches. See Davis serial protocol documentation.
+        :return: add a parameter with name measure and value "value" in degrees to the object
+        '''
         v = (value/1000.0)*25.4
-        self.addParameter(measure, v)
+        self._addParameter(measure, v)
